e4b19e61ff0a875ddc79c5e5ce8c358997836212
Zakaria El Alami <kiraelalami@gmail.com>
2024-11-02 16:32:02 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello, React!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <WelcomeMessage />
+        </div>
+    );
 }
 
 export default App;

b9f2468f74501561def93f2b77332a2e390a71a0
Zakaria El Alami <kiraelalami@gmail.com>
2024-11-09 00:16:12 +0100

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,12 +1,9 @@
-// src/components/MainContent.jsx
-import React from 'react';
-
-function MainContent() {
-    return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
-    );
-}
+const MainContent = () => {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f0f0f0', textAlign: 'center' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
 
 export default MainContent;

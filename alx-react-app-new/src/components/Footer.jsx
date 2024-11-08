fb0a96c1401d374898f182aa7e4cffd39671c4d9
Zakaria El Alami <kiraelalami@gmail.com>
2024-11-09 00:16:26 +0100

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,12 +1,9 @@
-// src/components/Footer.jsx
-import React from 'react';
-
-function Footer() {
-    return (
-        <footer>
-            <p>© 2023 City Lovers</p>
-        </footer>
-    );
-}
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: 'darkgray', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+};
 
 export default Footer;

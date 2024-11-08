7ac00dde77e992216c1e1095cb2d43853b1a0d53
Zakaria El Alami <kiraelalami@gmail.com>
2024-11-09 00:18:28 +0100

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,16 +1,15 @@
+// src/App.jsx
 import React from 'react';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
-import UserProfile from './UserProfile';
+import Counter from './components/Counter';  // Import the Counter component
 
 function App() {
   return (
-    <div>
-      <Header />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <MainContent />
-      <Footer />
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      {/* Add a heading to the app */}
+      <h1>Simple Counter Application</h1>
+      
+      {/* Render the Counter component */}
+      <Counter />
     </div>
   );
 }

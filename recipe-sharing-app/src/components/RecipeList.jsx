56cdfbb6b9432094b71c401bb9ca3c927d0d3c7d
Zakaria El Alami <kiraelalami@gmail.com>
2024-11-17 15:47:56 +0100

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import useRecipeStore from '../recipeStore'; // remove curly braces for default import
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import useRecipeStore from '../recipeStore';
 import FavoriteButton from './FavoriteButton';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes); // Use 'recipes' directly
+  const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
       {recipes.map((recipe) => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
+          <Link to={`/recipes/${recipe.id}`}> {/* Link to detailed view */}
+            <h3>{recipe.title}</h3>
+          </Link>
           <p>{recipe.description}</p>
           <FavoriteButton recipeId={recipe.id} />
         </div>

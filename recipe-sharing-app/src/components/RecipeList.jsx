b3037fd36259240ef3fe428d35580174c9b17d1b
Zakaria El Alami <kiraelalami@gmail.com>
2024-11-17 15:39:15 +0100

    Create RecipeList.jsx

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useRecipeStore } from '../recipeStore';
+import FavoriteButton from './FavoriteButton';
+
+const RecipeList = () => {
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+
+  return (
+    <div>
+      {filteredRecipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+          <FavoriteButton recipeId={recipe.id} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;

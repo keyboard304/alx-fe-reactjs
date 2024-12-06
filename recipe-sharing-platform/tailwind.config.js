dee7f2671bd53cb7b709739e6f7056d5d4abe907
Zakaria El Alami <kiraelalami@gmail.com>
2024-12-06 18:55:51 +0100

    Update tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Updated property
+  darkMode: false, // You can also set this to 'media' or 'class' if needed
   theme: {
     extend: {},
   },

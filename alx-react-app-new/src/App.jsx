8cbbef0c8e963632213debaa6b87790203d13008
keyboard304 <123272222+keyboard304@users.noreply.github.com>
2025-09-08 03:16:27 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,17 +1,21 @@
 // src/App.jsx
 import React from 'react';
-import Counter from './components/Counter';  // Import the Counter component
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+import WelcomeMessage from './WelcomeMessage';
 
 function App() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      {/* Add a heading to the app */}
-      <h1>Simple Counter Application</h1>
-      
-      {/* Render the Counter component */}
-      <Counter />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <WelcomeMessage />
+            <MainContent />
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;

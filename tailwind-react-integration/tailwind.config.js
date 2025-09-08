c04226ad19bf646fce1e362df4dc64d4b65e83ea
keyboard304 <123272222+keyboard304@users.noreply.github.com>
2025-09-08 04:15:46 +0300

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,8 @@
-export const purge = ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"];
-export const darkMode = false;
-export const theme = {
-  extend: {},
-};
-export const variants = {
-  extend: {},
-};
-export const plugins = [];
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
